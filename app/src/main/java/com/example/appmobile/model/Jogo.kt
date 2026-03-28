9cc6b81894530a5e3c895311a2c5816c16564626
Laysa <laysacibele23@gmail.com>
2026-03-28 14:33:31 -0300

    feat: criando as models (Usuario, Jogo e Biblioteca)

---
+++ app/src/main/java/com/example/appmobile/model/Jogo.kt
@@ -0,0 +1,11 @@
+package com.example.appmobile.model
+
+data class Jogo(
+    val id: String = "",
+    val nome: String = "",
+    val descricao: String = "",
+    val genero: String= "",
+    val plataformas: String= "",
+    val capaImagem: String = "",
+    val linkWiki: String = ""
+)
---
+++ app/src/main/java/com/example/appmobile/model/Usuario.kt
@@ -0,0 +1,8 @@
+package com.example.appmobile.model
+
+data class Usuario (
+    val uid: String = "",
+    val nome: String = "",
+    val email: String = "",
+    val senha: String = ""
+)
